08445dc291a37ff576b7ed15c50851c7bddcbfca
jujbob@gmail.com <jujbob@gmail.com>
2016-02-27 20:37:56 +0900

    adding for readme

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
@@ -9,7 +8,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(
         default=timezone.now)
     published_date = models.DateTimeField(
-        blank=True, null=True)
+        blank=True, null=True);
 
     def publish(self):
         self.published_date = timezone.now()
