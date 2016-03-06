16e2aef39c617dc7a04fb4a69653e7bd370d1e92
jujbob@gmail.com <jujbob@gmail.com>
2016-03-06 21:48:53 +0900

    fix after login view

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+                       url(r'^accounts/login/$', 'django.contrib.auth.views.login',),
+                       url(r'^accounts/profile/$', 'blog.views.post_list',),
                        url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
                        url(r'', include('blog.urls')),
 )
