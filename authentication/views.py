9e7c8ca266dade4dc02811fda93a828d7821f21c
kyungtae <jujbob@gmail.com>
2016-04-08 00:13:38 +0900

    Adding authentication views for rest framework

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import permissions, viewsets
+from authentication.models import Account
+from authentication.serializers import AccountSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
+    lookup_field = 'username'
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(),)
+
+        if self.request.method == 'POST':
+            return (permissions.AllowAny(),)
+
+        return (permissions.IsAuthenticated(),  )
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
 from blog import views
+from authentication.views import AccountViewSet
 
 router = routers.DefaultRouter()
-#router.register(r'api/users', views.UserViewSet)
+router.register(r'api/users', AccountViewSet)
 router.register(r'api/posts', views.PostViewSet)
 router.register(r'api/comments', views.CommentViewSet)
 router.register(r'api/subComments', views.SubCommentViewSet)
