ac9074f19da5a0ee7baa5843b0060cdb443656ec
kyungtae <jujbob@gmail.com>
2016-05-13 22:05:07 +0900

    error correction

--- authentication/urls.py
+++ authentication/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns(
     url(r'^api/', include(router.urls)),
     url(r'^auth/login_view/$', views.login_view, name='login_view'),
     url(r'^auth/login_user/$', views.login_user, name='login_user'),
-    url(r'^account/login/?*$', views.login_user, name='login_user'),
+#    url(r'^account/login/?*$', views.login_user, name='login_user'),
     url(r'^auth/logout_user/$', views.logout_user, name='logout_user')
     #url(r'^account/logout/$', views.subComment_remove, name='subComment_remove')
 
